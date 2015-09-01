d953e4308668ace7a31233046143004649afcda6
OscarJusto <justo_mtz4@hotmail.com>
2015-09-01 16:42:54 -0500

    Agua_Agregar estructura del menu_principal

--- Agua/Agua.ino
+++ Agua/Agua.ino
@@ -1,3 +1,23 @@
+#include <MenuSystem.h>
+
+MenuSystem menu_principal;
+Menu menu_info                ("Info estanques >");
+MenuItem mi_estanque_1        ("   Estanque 1  >");
+MenuItem mi_estanque_2        ("<  Estanque 2  >");
+MenuItem mi_estanque_3        ("<  Estanque 3  >");
+MenuItem mi_estanque_4        ("<  Estanque 4  >");
+Menu menu_info_pH             ("< Sensor de pH >");
+MenuItem mi_pH_1              ("<< Estanque 1 >>");
+MenuItem mi_pH_2              ("<< Estanque 2 >>");
+MenuItem mi_pH_3              ("<< Estanque 3 >>");
+MenuItem mi_pH_4              ("<< Estanque 4   ");
+Menu menu_calibrar            ("<  Calibrar     ");
+MenuItem mc_sensor_1          ("<<     pH     >>");
+MenuItem mc_sensor_2          ("<<     OD 1   >>");
+MenuItem mc_sensor_3          ("<<     OD 2   >>");
+MenuItem mc_sensor_4          ("<<     OD 3   >>");
+MenuItem mc_sensor_5          ("<<     OD 4     ");
+
 void setup() {
   // put your setup code here, to run once:
 
