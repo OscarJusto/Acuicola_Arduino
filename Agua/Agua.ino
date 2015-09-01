8187cbc6bb3507c7c1d98c4015ac44dc2a05400d
OscarJusto <justo_mtz4@hotmail.com>
2015-09-01 16:49:50 -0500

    Agua_Configurar y relacionar partes del menu

--- Agua/Agua.ino
+++ Agua/Agua.ino
@@ -18,9 +18,83 @@ MenuItem mc_sensor_3          ("<<     OD 2   >>");
 MenuItem mc_sensor_4          ("<<     OD 3   >>");
 MenuItem mc_sensor_5          ("<<     OD 4     ");
 
-void setup() {
-  // put your setup code here, to run once:
+void on_item1_selected(MenuItem* p_menu_item) {
+  Serial.println("PH");
+}
 
+void on_item2_selected(MenuItem* p_menu_item) {
+  Serial.println("OD1_TEM1");
+}
+
+void on_item3_selected(MenuItem* p_menu_item) {
+  Serial.println("OD2_TEM2");
+}
+
+void on_item4_selected(MenuItem* p_menu_item) {
+  Serial.println("OD3_TEM3");
+}
+
+void on_item5_selected(MenuItem* p_menu_item) {
+  Serial.println("OD4_TEM4");
+}
+
+void on_cal1_selected(MenuItem* p_menu_item) {
+  Serial.println("CAL_PH"); 
+}
+
+void on_cal2_selected(MenuItem* p_menu_item) {
+  Serial.println("CAL_OD1");
+}
+
+void on_cal3_selected(MenuItem* p_menu_item) {
+  Serial.println("CAL_OD2");
+}
+
+void on_cal4_selected(MenuItem* p_menu_item) {
+  Serial.println("CAL_OD3");
+}
+void on_cal5_selected(MenuItem* p_menu_item) {
+  Serial.println("CAL_OD4");
+}
+
+void configurar_menu () {
+    
+  Serial.println("Construyendo menu.");
+  
+  menu_info_pH.add_item(&mi_pH_1, &on_item1_selected);
+  menu_info_pH.add_item(&mi_pH_2, &on_item1_selected);
+  menu_info_pH.add_item(&mi_pH_3, &on_item1_selected);  
+  menu_info_pH.add_item(&mi_pH_4, &on_item1_selected);  
+  
+  menu_info.add_menu(&menu_info_pH);
+  menu_info.add_item(&mi_estanque_1, &on_item2_selected);
+  menu_info.add_item(&mi_estanque_2, &on_item3_selected);
+  menu_info.add_item(&mi_estanque_3, &on_item4_selected);
+  menu_info.add_item(&mi_estanque_4, &on_item5_selected);
+  
+  menu_info.add_menu(&menu_calibrar);
+  menu_calibrar.add_item(&mc_sensor_1, &on_cal1_selected);
+  menu_calibrar.add_item(&mc_sensor_2, &on_cal2_selected);
+  menu_calibrar.add_item(&mc_sensor_3, &on_cal3_selected);
+  menu_calibrar.add_item(&mc_sensor_4, &on_cal4_selected);
+  menu_calibrar.add_item(&mc_sensor_5, &on_cal5_selected);
+  
+  menu_calibrar.add_menu(&menu_info_pH);
+  
+  menu_principal.set_root_menu(&menu_info);
+  
+  Serial.println("Menu construido.");
+  
+}
+
+
+
+void setup() {
+  
+  Serial.begin(9600); 
+       
+  configurar_menu();
+    
 }
 
 void loop() {
